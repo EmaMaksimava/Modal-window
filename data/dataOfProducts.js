77f9315b1697278802d6970f3726fc9621f99643
Emma Maksimava <cielvibritania@gmail.com>
2022-11-18 12:07:57 +0100

    feat: update data base of products

--- data/dataOfProducts.js
+++ data/dataOfProducts.js
@@ -5,5 +5,40 @@ const dataOfProducts = [
     info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
     image: 'https://buymassry.com/images/thumbnails/400/400/detailed/19/Orange_Navel_Agreen.jpg',
     country: 'Spain'
-  }
+  },
+  {
+    name: 'Lime',
+    price: '11.49',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
+    image: 'http://buymassry.com/images/detailed/21/Lime.jpg',
+    country: 'Egypt'
+  },
+  {
+    name: 'Lichi',
+    price: '18.99',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
+    image: 'https://yesfrukt.com/storage/source/dfab9c783ef4cdcbad49eaa67c6f8310/product/1/hcRAPtIzWwAmg48nqO1mMGraTwJCo_zM.jpg',
+    country: 'China'
+  },
+  {
+    name: 'Mango',
+    price: '8.99',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
+    image: 'https://st2.depositphotos.com/1036708/10793/i/950/depositphotos_107939344-stock-photo-mango-isolated-on-white.jpg?forcejpeg=true',
+    country: 'Egypt'
+  },
+  {
+    name: 'Pitaya',
+    price: '19.99',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
+    image: 'https://msk.mangolavka.com/upload/resize_cache/webp/iblock/79f/79f1878276fb778c6741bb9b1ed778f7.webp',
+    country: 'Tailand'
+  },
+  {
+    name: 'Peach',
+    price: '6.99',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sequi fuga nostrum!',
+    image: 'https://img.freepik.com/premium-photo/fresh-peach-fruits-and-half_299651-1751.jpg',
+    country: 'Spain'
+  },
 ]
